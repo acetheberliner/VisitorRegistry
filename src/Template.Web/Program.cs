84d02aecbc85812054f30aeca4982a9a1e28b960
acetheberliner <tommaso.bagnolini@studio.unibo.it>
2025-11-05 13:14:50 +0100

    Reduced noisy logs and db init corrections

--- src/Template.Web/Program.cs
+++ src/Template.Web/Program.cs
@@ -1,25 +1,57 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Template.Web
 {
+    // Classe per l'avvio dell'applicazione web
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var hostBuilder = CreateHostBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    // Pulisce i log predefiniti e aggiunge il log su console
+                    logging.ClearProviders();
+                    logging.AddConsole();
+
+                    // Imposta il livello minimo di log globale a Warning
+                    logging.SetMinimumLevel(LogLevel.Warning);
+
+                    // Permette i log di Template.Web a livello Info
+                    logging.AddFilter("Template.Web", LogLevel.Information);
+
+                    // Assicura che i log di EF e Microsoft siano a livello Warning o superiore
+                    logging.AddFilter("Microsoft", LogLevel.Warning);
+                    logging.AddFilter("Microsoft.EntityFrameworkCore", LogLevel.Warning);
+                });
+
+            var host = hostBuilder.Build();
+
+            // Avvia l'applicazione
+            host.Start();
+
+            // Logga gli indirizzi di ascolto una volta
+            var logger = host.Services.GetService(typeof(ILogger<Program>)) as ILogger;
+            var serverAddresses = host.Services.GetService(typeof(Microsoft.AspNetCore.Hosting.Server.Features.IServerAddressesFeature)) as Microsoft.AspNetCore.Hosting.Server.Features.IServerAddressesFeature;
+            if (logger != null && serverAddresses != null)
+            {
+                foreach (var address in serverAddresses.Addresses)
+                {
+                    logger.LogInformation("Applicazione in ascolto su: {address}", address);
+                }
+            }
+
+            // Attendi la chiusura
+            host.WaitForShutdown();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.ConfigureKestrel(kestrel =>
-                    {
-                        kestrel.AddServerHeader = false; // OWASP: Remove Kestrel response header 
-                    });
-
                     webBuilder.UseStartup<Startup>();
-                });
+                }
+            );
     }
 }
