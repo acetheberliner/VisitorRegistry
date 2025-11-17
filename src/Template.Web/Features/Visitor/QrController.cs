13a6cdcc6f306bd6ffb2e97d75a20e853adea7bf
acetheberliner <tommaso.bagnolini@studio.unibo.it>
2025-11-17 12:17:00 +0100

    Compatibility updates to run the project on mac also

--- src/Template.Web/Features/Visitor/QrController.cs
+++ src/Template.Web/Features/Visitor/QrController.cs
@@ -1,9 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using QRCoder;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Runtime.Versioning;
 
 namespace Template.Web.Features.Visitor
 {
@@ -12,7 +8,6 @@ namespace Template.Web.Features.Visitor
     {
         // GET /qr/generate?key=demo-qr
         [HttpGet("generate")]
-        [SupportedOSPlatform("windows")] // <-- indica che questo metodo usa API supportate solo su Windows (risolve CA1416)
         public virtual IActionResult Generate(string key, string baseUrl = null, bool debug = false)
         {
             if (string.IsNullOrWhiteSpace(key)) return BadRequest("Missing key");
@@ -28,18 +23,11 @@ namespace Template.Web.Features.Visitor
 
             using (var qrGenerator = new QRCodeGenerator())
             using (var qrData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q))
-            using (var qrCode = new QRCode(qrData))
+            using (var qrCode = new PngByteQRCode(qrData))
             {
-                // Colore brandizzato onit: dark = #47474b
-                var dark = ColorTranslator.FromHtml("#47474b");
-                var light = Color.White;
-
-                using (var bitmap = qrCode.GetGraphic(20, dark, light, true))
-                using (var ms = new MemoryStream())
-                {
-                    bitmap.Save(ms, ImageFormat.Png);
-                    return File(ms.ToArray(), "image/png");
-                }
+                // Usa PngByteQRCode per compatibilità cross-platform (senza System.Drawing)
+                var bytes = qrCode.GetGraphic(20);
+                return File(bytes, "image/png");
             }
         }
 
